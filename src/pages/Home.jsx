f4fa7b5154f8212006d7ca9a5396f7d332b17e38
DiegoRodriguez <diegordriguezt@gmail.com>
2025-11-04 08:33:53 -0600

    Formateo del codigo

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,51 +4,48 @@ import { getPopularMovies } from "../api/tmdb";
 import MovieList from "../components/MovieList";
 
 function Home() {
-    const [movies, setMovies] = useState([]);
-    const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
+  useEffect(() => {
+    loadMovies(page);
+  }, [page]);
 
-
-    useEffect(() => {
-        loadMovies(page);
-    }, [page]);
-
-    async function loadMovies(pageNum) {
-        try {
-            setLoading(true);
-            const newMovies = await getPopularMovies(pageNum);
-                setMovies(prev => {
-  const allMovies = [...prev, ...newMovies];
-  return allMovies.filter(
-    (movie, index, self) =>
-      index === self.findIndex(m => m.id === movie.id)
-  );
-});
-        } catch (err) {
-            setError(`Hubo problemas al cargar las peliculas: ${err.message}`);
-        } finally {
-            setLoading(false);
-        }
+  async function loadMovies(pageNum) {
+    try {
+      setLoading(true);
+      const newMovies = await getPopularMovies(pageNum);
+      setMovies((prev) => {
+        const allMovies = [...prev, ...newMovies];
+        return allMovies.filter(
+          (movie, index, self) =>
+            index === self.findIndex((m) => m.id === movie.id)
+        );
+      });
+    } catch (err) {
+      setError(`Hubo problemas al cargar las peliculas: ${err.message}`);
+    } finally {
+      setLoading(false);
     }
+  }
 
-    return (
-        <main>
-            <h1>Peliculas Populares</h1>
-            <MovieList movies={movies}/>
+  return (
+    <main> 
+      <h1>Peliculas Populares</h1>
+      <MovieList movies={movies} />
 
-            {loading && <p>Cargando más...</p>}
-            {error && <p>{error}</p>}
+      {loading && <p>Cargando más...</p>}
+      {error && <p>{error}</p>}
 
-<div className="btn">
-    <button onClick={() => setPage(p => p+1)} disabled={loading}>
-                Ver mas peliculas
-            </button>
-</div>
-            
-        </main>
-    );
+      <div className="btn">
+        <button onClick={() => setPage((p) => p + 1)} disabled={loading}>
+          Ver mas peliculas
+        </button>
+      </div>
+    </main>
+  );
 }
 
 export default Home;
